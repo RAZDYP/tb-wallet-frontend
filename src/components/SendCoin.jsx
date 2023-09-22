955faf5aa338bd81a947a2eb8503c1029a5bf591
Rajdeep nath <rajdeeprajdeep03@gmail.com>
2023-09-22 21:13:31 +0530

    feat: added send coin function

--- src/components/SendCoin.jsx
+++ src/components/SendCoin.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileIcone from '../images/profile.png';
 import bellIcon from '../images/Notification.png';
 import NavTopProfile from './NavTopProfile';
 
-function SendCoin() {
+function SendCoin(props) {
+
+    const [bitcoinAddress, setBitcoinAddress] = useState('')
+    const [amount, setAmount] = useState('')
+
+    const handleSendBitcoin = async () => {
+        const response = await fetch(`http://127.0.0.1:3000/api/wallet/debitBalance`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                bitAddress: bitcoinAddress,
+                btcBalance: amount,
+            })
+        })
+
+        const result = await response.json()
+        console.log(result)
+        alert(result.message)
+    }
+
     return (
         <>
             <div className='col-md-9 p-3 font-style-verdana'>
-                <NavTopProfile />
+                <NavTopProfile user={props.user} />
                 <div className='w-100 mt-5'>
                     <h3 className='w-100 text-center fw-bold font-style-verdana'>Send BTC</h3>
                 </div>
                 <div className='w-100 mt-5 d-flex align-items-center justify-content-center'>
                     <div className='card w-50 py-4 border-0 rounded-5' style={{ backgroundColor: "#C2C0FF1F" }}>
                         <div className='card-body px-4 py-5' >
-                            <input type='text' className='form-control mb-4 p-3 rounded-3' style={{ color: "#B8B8B8" }} placeholder='Bitcoin Address' />
-                            <input type='number' className='form-control mb-5 p-3 rounded-3' style={{ color: "#B8B8B8" }} placeholder='Enter Amount' />
-                            <button className='btn btn-dark w-100 p-3 rounded-3 border-0 ' style={{ color: "white", backgroundColor: "#F80F0F" }}>Send</button>
+                            <input type='text' className='form-control mb-4 p-3 rounded-3' style={{ color: "#B8B8B8" }} placeholder='Bitcoin Address'
+                                onChange={(e) => setBitcoinAddress(e.target.value)} value={bitcoinAddress}
+                            />
+                            <input type='number' className='form-control mb-5 p-3 rounded-3' style={{ color: "#B8B8B8" }} placeholder='Enter Amount'
+                                onChange={(e) => setAmount(e.target.value)} value={amount}
+                            />
+                            <button className='btn btn-dark w-100 p-3 rounded-3 border-0 ' style={{ color: "white", backgroundColor: "#F80F0F" }}
+                                onClick={handleSendBitcoin}
+                            >Send</button>
                         </div>
                     </div>
                 </div>
--- src/pages/sendCoinPage.js
+++ src/pages/sendCoinPage.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SideMenu from '../components/SideMenu'
 import SendCoin from '../components/SendCoin'
 
 
 function SendCoinPage() {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        const email = localStorage.getItem('email')
+        const getUser = async () => {
+            const response = await fetch(`http://127.0.0.1:3000/api/users/find`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email })
+            })
+
+            const result = await response.json()
+            console.log(result)
+            // setUser(result.user)
+
+        }
+
+        getUser()
+        // setUser(result.user)
+    }, [])
+
+    const cointypeList = [
+        { id: 1, name: 'BTC' },
+        { id: 2, name: 'ETH' },
+    ]
+
     return (
         <div className='col-md-12 d-flex'>
             <SideMenu />
-            <SendCoin />
+            <SendCoin user={user} />
         </div>
     )
 }
